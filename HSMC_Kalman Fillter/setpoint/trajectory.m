ea3aa9f4b77d2f11b1da698abd5a7f798eb0f998
Advanced Robotics and Automation Lab <43550092+aralab-unr@users.noreply.github.com>
2024-05-06 22:11:55 +0700

    Add files via upload

---
+++ HSMC_Kalman Fillter/setpoint/trajectory.m
@@ -0,0 +1,22 @@
+function trajec=trajectory(t)
+trajec=[12;12;12;0.5];
+%trajec=[sin(pi*t/10);-1+cos(pi*t/10);t;0];
+%if t<10
+%    trajec=[3;3;3;0.2];
+%end
+%if 10<= t && t <20
+%    trajec=[1.5;3;3;0.2];
+%end
+%if 20<= t && t <30
+%    trajec=[1.5;1.5;3;0.2];
+%end
+%if 30<= t && t <40
+%    trajec=[3;1.5;3;0.4];
+%end
+%if 40<= t && t <50
+%    trajec=[3;3;3;0.4];
+%end
+%if 50<= t 
+%    trajec=[3;3;0.01;0.4];
+%end
+end
